88e11d29c794ed4f6a3252875a7fca6f0102de28
Marco Gabel <marcgabel@live.dk>
2021-03-02 15:14:03 +0100

    GUI

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -1,16 +1,11 @@
 package rps.gui.controller;
 
-// Java imports
+
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
-/**
- *
- * @author smsj
- */
 public class GameViewController implements Initializable {
 
     public ImageView startImage;
