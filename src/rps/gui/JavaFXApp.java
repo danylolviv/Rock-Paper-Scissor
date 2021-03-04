06db992dfeb99a131c45230af96470fa3cef2d95
tomaspeniak <tomaspeniak@users.noreply.github.com>
2021-03-04 23:28:31 +0100

    History is white now

--- src/rps/gui/JavaFXApp.java
+++ src/rps/gui/JavaFXApp.java
@@ -98,6 +98,7 @@ public class JavaFXApp extends Application {
         ge.getLastResult();
         javafx.scene.control.Label lblContent = new javafx.scene.control.Label(getResultAsString(ge.getLastResult()));
         VBox vbox = new VBox();
+        lblContent.setStyle("-fx-text-fill: #FFFFFF");
         vbox.getChildren().add(lblContent);
         tilePaneClass.getChildren().add(vbox);
         scrollPane.setContent(tilePaneClass);
